3b55bea6b8ab652dfeaaed62a14c315fb7474cad
spiderasasen <spiderasasen@gmail.com>
2026-03-13 08:45:41 -0400

    finished the main layout made a TODO to let me know what to do on the next section of this project

--- Converter_Expo/app/(tabs)/index.tsx
+++ Converter_Expo/app/(tabs)/index.tsx
@@ -1,15 +1,44 @@
-import React from 'react';
-import {useState} from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+import DropDownPicker from 'react-native-dropdown-picker';
 
 export default function App() {
-  const [country, setCountry] = useState();
+  const [open, setOpen] = useState(false);
+  const [country, setCountry] = useState(null);
+  const [items, setItems] = useState([
+    { label: "United States", value: "US" },
+    { label: "Canada", value: "CA" },
+    { label: "Mexico", value: "MX" }
+  ]);
 
   return (
       <View style={styles.container}>
         <Text style={styles.title}>Converter App</Text>
-        <Text style={styles.subtitle}>Please Select a Country</Text>
+        <Text style={styles.subtitle}>Converting all the essentials in 1 app</Text>
 
+        {/*dropdown bar being created*/}
+        <View style={styles.dropdownWrapper}>
+          <DropDownPicker
+              open={open}
+              value={country}
+              items={items}
+              setOpen={setOpen}
+              setValue={setCountry}
+              setItems={setItems}
+              searchable={true}
+              placeholder="Search or select a country"
+              style={styles.dropdown}
+              dropDownContainerStyle={styles.dropdown}
+          />
+        </View>
+
+        {/*TODO: once the user clicks on the button, it will send the info to another screen*/}
+        {/*<Button title={"convert"} >Convert</Button>*/}
+
+        {/*this right now is a tester*/}
+        {country && (
+            <Text style={styles.selectedText}>Selected: {country}</Text>
+        )}
       </View>
   );
 }
@@ -30,4 +59,20 @@ const styles = StyleSheet.create({
     marginTop: 10,
     color: '#555',
   },
+  selectedText: {
+    marginTop: 20,
+    fontSize: 16,
+  },
+  dropdownWrapper: {
+    width: "100%",
+    alignItems: "center",
+    marginTop: 20,
+  },
+
+  dropdown: {
+    width: 250,
+    alignSelf: "center",
+  },
+
+
 });

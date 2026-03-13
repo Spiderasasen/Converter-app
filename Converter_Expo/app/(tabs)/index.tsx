48581ae2a3dcfd93102370f721553bd93258552f
spiderasasen <spiderasasen@gmail.com>
2026-03-13 08:13:30 -0400

    before i make some search engine system

--- Converter_Expo/app/(tabs)/index.tsx
+++ Converter_Expo/app/(tabs)/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import {useState} from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 export default function App() {
+  const [country, setCountry] = useState();
+
   return (
       <View style={styles.container}>
         <Text style={styles.title}>Converter App</Text>
-        <Text style={styles.subtitle}>Welcome! Let's build this.</Text>
+        <Text style={styles.subtitle}>Please Select a Country</Text>
+
       </View>
   );
 }
